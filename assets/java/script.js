8cb77ead01ae1656c32a001d47b78845260efb32
Aymane Hassani <Aymane.hassani@outlook.fr>
2024-08-03 13:55:44 +0100

    added humberger menu

---
+++ assets/java/script.js
@@ -0,0 +1,10 @@
+// script.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.getElementById('menu-icon');
+    const navMenu = document.getElementById('nav-menu');
+
+    menuIcon.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+    });
+});
